aada695be52c22c9dc90bf7507604987b754e1cd
Tyler Unger <ungert@hawaii.edu>
2026-03-28 09:32:18 -1000

    Round 1
    Round 1 16mins

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,38 @@
-import { Col, Container, Image, Row } from 'react-bootstrap';
+import { Col, Container, Row } from 'react-bootstrap';
+import { PeopleFill, FileEarmarkTextFill, Calendar2CheckFill } from 'react-bootstrap-icons';
 
 /** The Home page. */
 const Home = () => (
   <main>
-    <Container id="landing-page" fluid className="py-3">
-      <Row className="align-middle text-center">
-        <Col xs={4}>
-          <Image src="next.svg" width="150px" alt="" />
+    <Container className="py-5">
+      <Row>
+
+        <Col className="text-center">
+          <PeopleFill size={100} />
+          <h1>Manage Contacts</h1>
+          <h5>
+            This address book enables any number of users to register and save their business contacts.
+            You can only see the contacts you have created.
+          </h5>
         </Col>
 
-        <Col xs={8} className="d-flex flex-column justify-content-center">
-          <h1>Welcome to this template</h1>
-          <p>Now get to work and modify this app!</p>
+        <Col className="text-center">
+          <FileEarmarkTextFill size={100} />
+          <h1>Store Information</h1>
+          <h5>
+            For each contact, you can save their name, address, and phone number.
+          </h5>
         </Col>
+
+        <Col className="text-center">
+          <Calendar2CheckFill size={100} />
+          <h1>Track Interactions</h1>
+          <h5>
+            Each time you make contact with a contact, you can write a note that summarizes the conversation.
+            This note is saved along with a timestamp with the contact.
+          </h5>
+        </Col>
+
       </Row>
     </Container>
   </main>
